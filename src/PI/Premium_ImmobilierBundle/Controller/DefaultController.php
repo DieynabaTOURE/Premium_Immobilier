1db10ee1a0673a4935f5cf2a1b9aed5b333e2c4a
Prince <Abdoulaye Faye>
2018-02-12 10:52:33 +0000

    first commit

---
+++ src/PI/Premium_ImmobilierBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PI\Premium_ImmobilierBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('PremiumBundle:Default:index.html.twig');
+    }
+}
